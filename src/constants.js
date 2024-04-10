2cfeeea5454728608f65506daaa4a6f0b1b6b099
Vaibhav Sharma <vaibhavsharmsk125@gmail.com>
2024-04-11 03:39:34 +0530

    Your descriptive commit message

--- src/constants.js
+++ src/constants.js
@@ -4,13 +4,13 @@ export const dialogueData = {
   pc: `This is my PC. I work mostly in Python these days.
      I also like Javascript. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/VAIBHAV13S" target="_blank">Github</a>!`,
-  "Unknown": `I don't know about this frame.Maybe, together we will be able to solve this mystery.`,
+    "Unknown": `I don't know what this is. Maybe we should tpgether be able to solve this mystery!`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube and Movies.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/VAIBHAV13S/Resume/blob/main/Resume.pdf" target="_blank">Check it out?</a>`,
   projects: `Info about this PlayFolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
   library: `There are a lot of programming books on my shelves.
